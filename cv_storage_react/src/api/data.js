9edb20e60ec06596bc92faf8b7eab6d9242f457c
l1st1k <listik.mp@gmail.com>
2023-03-27 13:38:54 +0300

    added cv_data

--- cv_storage_react/src/api/data.js
+++ cv_storage_react/src/api/data.js
@@ -1,5 +1,49 @@
 cvs = [
     {
-
+        "cv_id": "c59e21a0-effa-4f69-90b1-8f1bc58cf4f7",
+        "first_name": "Aliaksandr",
+        "last_name": "Listvanovich",
+        "age": 18,
+        "major": "DevOps",
+        "years_of_exp": 7
+    },
+    {
+        "cv_id": "21c060bc-5aeb-4109-a2d9-419a88644dd8",
+        "first_name": "Daniil",
+        "last_name": "Boloshenko",
+        "age": 45,
+        "major": "frontend engineer",
+        "years_of_exp": 1
+    },
+    {
+        "cv_id": "98bfc682-a6df-463d-bbab-7db444ee3c4f",
+        "first_name": "Ihor",
+        "last_name": "Zhuk",
+        "age": 56,
+        "major": "DevOps",
+        "years_of_exp": 7
+    },
+    {
+        "cv_id": "01a32924-a64c-41a0-acc3-c36e17b592e1",
+        "first_name": "Ehor",
+        "last_name": "Zavadskiy",
+        "age": 5,
+        "major": "cartoon developer",
+        "years_of_exp": 3
     }
 ]
+
+
+
+single_cv_example = {
+    "cv_id": "21c060bc-5aeb-4109-a2d9-419a88644dd8",
+    "first_name": "Daniil",
+    "last_name": "Boloshenko",
+    "age": 45,
+    "major": "frontend engineer",
+    "years_of_exp": 1,
+    "phone_number": "+375294589925",
+    "skills": "js CSS",
+    "projects": "finance django website",
+    "project_amount": 2
+}
