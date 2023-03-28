625d24d26551faf8a14dd84087886ed7fcf8a015
l1st1k <listik.mp@gmail.com>
2023-03-29 01:19:33 +0300

    Installed zustand. Added store

--- cv_storage_react/src/API/delete_cv.js
+++ cv_storage_react/src/API/delete_cv.js
@@ -1,20 +1,15 @@
 import api from "./api_base.js";
-import cvs from "./data.js"
 
 const delete_cv = async (cv_id) => {
-    // try {
-    //     return await api
-    //         .get("cvs")
-    //         .json();
-    // } catch (err) {
-    //     //TODO custom alert on error
-    //     console.log(err.message)
-    // }
-    cvs.cvs.reduce((previousValue, currentValue) => {
-        if (currentValue.cv_id !== cv_id) {
-            previousValue.push(currentValue)
-        }
-    }, [])
+    try {
+        let response = await api.delete(`cv/${cv_id}`);
+        //TODO check for deletion status code, if its 200
+        if (response.status !== 200) throw Error(await response.json());
+        return response.json();
+    } catch (err) {
+        //TODO custom alert on error
+        console.log(err.message)
+    }
 }
 
 export default delete_cv;
--- cv_storage_react/src/API/get_all_cvs.js
+++ cv_storage_react/src/API/get_all_cvs.js
@@ -2,9 +2,9 @@ import api from "./api_base.js";
 
 const get_all_cvs = async () => {
     try {
-        return await api
-            .get("cvs")
-            .json();
+        let response = await api.get("cvs");
+        if (response.status !== 200) throw Error(await response.json());
+        return response.json();
     } catch (err) {
         //TODO custom alert on error
         console.log(err.message)
