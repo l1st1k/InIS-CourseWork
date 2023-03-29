27670c781349ee8b4f2e8f50283a2db72cf116e2
l1st1k <listik.mp@gmail.com>
2023-03-29 16:13:50 +0300

    new GitHubButton.jsx

--- cv_storage_react/src/components/Buttons/GitHubButton.jsx
+++ cv_storage_react/src/components/Buttons/GitHubButton.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import {IconButton} from "@mui/material";
+import {Button, IconButton} from "@mui/material";
 
-const GitHubButton = () => {
+const GitHubButton = (props) => {
+    const { name, link } = props;
+
     return (
-        <IconButton href="https://github.com/l1st1k" target="_blank">
-            <GitHubIcon />
-        </IconButton>
+        <Button
+            variant={'contained'}
+            startIcon={<GitHubIcon />}
+            size={'small'}
+            href={link}
+            target="_blank"
+            color={'secondary'}
+        >
+            {name}
+        </Button>
     );
+};
+
+GitHubButton.propTypes = {
+    name: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
 };
 
 export default GitHubButton;
