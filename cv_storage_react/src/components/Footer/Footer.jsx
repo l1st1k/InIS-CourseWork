749839e11f732660f3d1f4efb1a7c3d51da7e64c
l1st1k <listik.mp@gmail.com>
2023-03-31 16:36:08 +0300

    Updated Footer.jsx with new logo_on_white.svg

--- cv_storage_react/src/components/Footer/Footer.jsx
+++ cv_storage_react/src/components/Footer/Footer.jsx
@@ -1,30 +1,24 @@
-import React from 'react';
+import React from "react";
 import Copyright from "./Copyright.jsx";
-import {Box, Container, Typography} from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import FooterLinks from "./FooterLinks.jsx";
+import Logo from "../Logo.jsx";
 
 const Footer = () => {
-    return (
-        <Box
-            className={'footer'}
-            component="footer"
-        >
-            <Container
-                maxWidth="lg"
-                sx = {{mt: 3, py: 1}}
-            >
-                <Typography
-                    variant="h6"
-                    align="center"
-                    gutterBottom
-                >
-                    CV Storage
-                </Typography>
-                <FooterLinks/>
-                <Copyright />
-            </Container>
-        </Box>
-    );
+  return (
+    <Box className={"footer"} component="footer">
+      <Container maxWidth="lg" sx={{ mt: 3, py: 2 }}>
+        <Stack className={"footer-stack"} direction={"row"} spacing={2}>
+          <Logo />
+          <Typography variant="h6" align="center" gutterBottom>
+            CV Storage
+          </Typography>
+        </Stack>
+        <FooterLinks />
+        <Copyright />
+      </Container>
+    </Box>
+  );
 };
 
 export default Footer;
