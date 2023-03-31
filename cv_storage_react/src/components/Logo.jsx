7dcb9ff422448fbc90e2e8def6769c4d5704d905
l1st1k <listik.mp@gmail.com>
2023-03-31 16:35:31 +0300

    Added LogoOnWhite as component

---
+++ cv_storage_react/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as LogoOnWhite } from "../assets/img/logo_on_white.svg";
+
+const Logo = () => {
+  return (
+    <>
+      <LogoOnWhite />
+    </>
+  );
+};
+
+export default Logo;
