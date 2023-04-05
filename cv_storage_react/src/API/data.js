a914ded231686b2d2ff1de0bde03d88cb18a4ec6
l1st1k <listik.mp@gmail.com>
2023-04-05 14:49:41 +0300

    added some more data

--- cv_storage_react/src/API/data.js
+++ cv_storage_react/src/API/data.js
@@ -1,59 +1,129 @@
 const cvs = [
-    {
-        "cv_id": "c59e21a0-effa-4f69-90b1-8f1bc58cf4f7",
-        "first_name": "Aliaksandr",
-        "last_name": "Listvanovich",
-        "age": 18,
-        "major": "DevOps",
-        "years_of_exp": 7
-    },
-    {
-        "cv_id": "21c060bc-5aeb-4109-a2d9-419a88644dd8",
-        "first_name": "Daniil",
-        "last_name": "Boloshenko",
-        "age": 45,
-        "major": "frontend engineer",
-        "years_of_exp": 1
-    },
-    {
-        "cv_id": "98bfc682-a6df-463d-bbab-7db444ee3c4f",
-        "first_name": "Ihor",
-        "last_name": "Zhuk",
-        "age": 56,
-        "major": "DevOps",
-        "years_of_exp": 0
-    },
-    {
-        "cv_id": "01a32924-a64c-41a0-acc3-c36e17b592e1",
-        "first_name": "Ehor",
-        "last_name": "Zavadskiy",
-        "age": 5,
-        "major": "cartoon developer",
-        "years_of_exp": 3
-    },
-    {
-        "cv_id": "01a32924-a64c-41a0-acc3-c36e17b000e1",
-        "first_name": "Julia",
-        "last_name": "Mazanchick",
-        "age": 21,
-        "major": "lobster po masti",
-        "years_of_exp": 8
-    }
+  {
+    cv_id: "c59e21a0-effa-4f69-90b1-8f1bc58cf4f7",
+    first_name: "Aliaksandr",
+    last_name: "Listvanovich",
+    age: 18,
+    major: "DevOps",
+    years_of_exp: 7,
+  },
+  {
+    cv_id: "21c060bc-5aeb-4109-a2d9-419a88644dd8",
+    first_name: "Daniil",
+    last_name: "Boloshenko",
+    age: 45,
+    major: "frontend engineer",
+    years_of_exp: 1,
+  },
+  {
+    cv_id: "98bfc682-a6df-463d-bbab-7db444ee3c4f",
+    first_name: "Ihor",
+    last_name: "Zhuk",
+    age: 56,
+    major: "DevOps",
+    years_of_exp: 0,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b592e1",
+    first_name: "Ehor",
+    last_name: "Zavadskiy",
+    age: 5,
+    major: "cartoon developer",
+    years_of_exp: 3,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b000e1",
+    first_name: "Julia",
+    last_name: "Mazanchick",
+    age: 21,
+    major: "lobster po masti",
+    years_of_exp: 8,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b000e1",
+    first_name: "Kto-to",
+    last_name: "kirinovich",
+    age: 21,
+    major: "lobster po masti",
+    years_of_exp: 8,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b000e1",
+    first_name: "sdfsdf",
+    last_name: "fdsdfsdf",
+    age: 21,
+    major: "lobster po masti",
+    years_of_exp: 8,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b000e1",
+    first_name: "wetetet",
+    last_name: "jhhgfd",
+    age: 21,
+    major: "lobster po masti",
+    years_of_exp: 8,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b000e1",
+    first_name: "eeeeeeeeeeee",
+    last_name: "eeeeeeeeeeeee",
+    age: 21,
+    major: "lobster po masti",
+    years_of_exp: 8,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b000e1",
+    first_name: "hhhhhhhhh",
+    last_name: "hhhhhhhhhh",
+    age: 21,
+    major: "lobster po masti",
+    years_of_exp: 8,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b000e1",
+    first_name: "uuuuuuuuu",
+    last_name: "uuuuuuuu",
+    age: 21,
+    major: "lobster po masti",
+    years_of_exp: 8,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b000e1",
+    first_name: "ttttttttt",
+    last_name: "tttttttttt",
+    age: 21,
+    major: "lobster po masti",
+    years_of_exp: 8,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b000e1",
+    first_name: "rrrrrrrrr",
+    last_name: "rrrrrrrrrrrrr",
+    age: 21,
+    major: "lobster po masti",
+    years_of_exp: 8,
+  },
+  {
+    cv_id: "01a32924-a64c-41a0-acc3-c36e17b000e1",
+    first_name: "wwwwwwwwwwwww",
+    last_name: "wwwwwwwwwwww",
+    age: 21,
+    major: "lobster po masti",
+    years_of_exp: 8,
+  },
 ];
 
-
-
 const single_cv_example = {
-    "cv_id": "21c060bc-5aeb-4109-a2d9-419a88644dd8",
-    "first_name": "Daniil",
-    "last_name": "Boloshenko",
-    "age": 45,
-    "major": "frontend engineer",
-    "years_of_exp": 1,
-    "phone_number": "+375294589925",
-    "skills": "js CSS",
-    "projects": "finance django website",
-    "project_amount": 2
+  cv_id: "21c060bc-5aeb-4109-a2d9-419a88644dd8",
+  first_name: "Daniil",
+  last_name: "Boloshenko",
+  age: 45,
+  major: "frontend engineer",
+  years_of_exp: 1,
+  phone_number: "+375294589925",
+  skills: "js CSS",
+  projects: "finance django website",
+  project_amount: 2,
 };
 
-export default {cvs, single_cv_example};
+export default { cvs, single_cv_example };
