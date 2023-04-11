8d96cc524691bc9fa32a21e41490c487d6bc4cdc
l1st1k <listik.mp@gmail.com>
2023-04-11 20:22:25 +0300

    CVShort.jsx new layout. Semantic updates

--- cv_storage_react/src/components/CV_Components/CVShort.jsx
+++ cv_storage_react/src/components/CV_Components/CVShort.jsx
@@ -1,27 +1,45 @@
 import React from "react";
 import { year_to_string } from "../../utils";
 import { DeleteButton, OpenButton, DownloadButton } from "../Buttons";
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 export const CVShort = ({ cv, number }) => {
   return (
     <Box
       component="article"
       key={cv.cv_id}
-      sx={{ padding: 2, width: 1, border: "2px solid #d2d2d2" }}
+      sx={{
+        padding: 2,
+        width: 1,
+        border: "2px solid #d2d2d2",
+        display: "flex",
+      }}
     >
-      <div className="cv__content">
-        <strong>
+      <Stack direction={"column"} alignItems="stretch" sx={{ flex: "8" }}>
+        <Typography variant="body1" align="left">
           {number}. {cv.first_name} {cv.last_name}, {cv.age}
-        </strong>
-        <div>{cv.major}</div>
-        <div>{year_to_string(cv.years_of_exp)}</div>
-      </div>
-      <div className="cv_buttons">
-        <OpenButton cv_id={cv.cv_id} />
-        <DownloadButton cv_id={cv.cv_id} />
-        <DeleteButton cv_id={cv.cv_id} />
-      </div>
+        </Typography>
+        <Typography variant="body1">{cv.major}</Typography>
+        <Typography variant="body2">
+          {year_to_string(cv.years_of_exp)}
+        </Typography>
+      </Stack>
+      <CV_Links cv_id={cv.cv_id} />
     </Box>
+  );
+};
+
+const CV_Links = ({ cv_id }) => {
+  return (
+    <Stack
+      direction={"row"}
+      spacing={1}
+      justifyContent={"end"}
+      sx={{ flex: "2" }}
+    >
+      <OpenButton cv_id={cv_id} />
+      <DownloadButton cv_id={cv_id} />
+      <DeleteButton cv_id={cv_id} />
+    </Stack>
   );
 };
