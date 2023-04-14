05c4c9526f62d3b3dc7b9aa7430b25fa5d374b5f
l1st1k <listik.mp@gmail.com>
2023-04-14 15:59:34 +0300

    comp fixes

--- cv_storage_react/src/components/Buttons/OpenButton.jsx
+++ cv_storage_react/src/components/Buttons/OpenButton.jsx
@@ -3,8 +3,8 @@ import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import { IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
 
-export const OpenButton = (cv_id) => {
-  const link = "/cv/".concat(cv_id.cv_id);
+export const OpenButton = ({ cv_id }) => {
+  const link = `/cv/${cv_id}`;
   return (
     <IconButton sx={{ color: "#d2d2d2" }}>
       <Link to={link} style={{ color: "inherit" }}>
