03a3c2159a8fcfb0654669168e9ed3d9e3ea8004
l1st1k <listik.mp@gmail.com>
2023-04-14 17:53:07 +0300

    Done HomePage.jsx

--- cv_storage_react/src/pages/HomePage.jsx
+++ cv_storage_react/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 
@@ -15,17 +15,26 @@ export const HomePage = () => {
         }}
       >
         <Header sx={{ flex: "2" }} />
-        <Typography
-          variant="h4"
-          sx={{
-            py: 3,
-            flex: "10 1",
-            justifyContent: "center",
-            textAlign: "center",
-          }}
-        >
-          Welcome page (future)
-        </Typography>
+        <Container maxWidth="md">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="#d2d2d2"
+            gutterBottom
+          >
+            Welcome to CV Storage!
+          </Typography>
+          <Typography variant="h5" align="center" color="#d2d2d2" paragraph>
+            Welcome to CV Storage - the ultimate solution for HR and recruitment
+            teams. Our user-friendly platform allows for the secure and
+            streamlined management of resumes, making it easy to store, organize
+            and manipulate CVs. With advanced encryption and GDPR compliance, CV
+            Storage offers a reliable and efficient way to find the perfect
+            candidate for any role. Sign up today and start streamlining your
+            recruitment process.
+          </Typography>
+        </Container>
         <Footer sx={{ flex: "2" }} />
       </Box>
     </>
