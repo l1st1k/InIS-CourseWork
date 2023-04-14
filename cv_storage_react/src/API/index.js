59519808515414b5ef1f4bc193a67b68ec6c4110
l1st1k <listik.mp@gmail.com>
2023-04-14 22:09:18 +0300

    added get_single_cv.js

--- cv_storage_react/src/API/index.js
+++ cv_storage_react/src/API/index.js
@@ -1,5 +1,6 @@
 export { api } from "./api_base.js";
 export { get_all_cvs } from "./get_all_cvs.js";
+export { get_single_cv } from "./get_single_cv.js";
 export { get_csv } from "./get_csv.js";
 export { delete_cv } from "./delete_cv.js";
 
