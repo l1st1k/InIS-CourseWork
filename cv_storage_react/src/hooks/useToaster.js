9bcd86abbe656e87caab97019847ffe0360db8d4
l1st1k <listik.mp@gmail.com>
2023-04-18 13:37:28 +0300

    useToaster template

---
+++ cv_storage_react/src/hooks/useToaster.js
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+import { toast } from "react-toastify";
+
+export const useToaster = async (func, success, failure, params = null) => {
+  const toastId = useRef(null);
+
+  return async () => {
+    toastId.current = toast.loading("Processing...", {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+
+    const response = await func(params);
+    response === "Failed"
+      ? toast.update(toastId.current, {
+          autoClose: 3000,
+          render: failure,
+          type: toast.TYPE.ERROR,
+          isLoading: false,
+        })
+      : toast.update(toastId.current, {
+          autoClose: 3000,
+          render: success,
+          type: toast.TYPE.SUCCESS,
+          isLoading: false,
+        });
+  };
+};
