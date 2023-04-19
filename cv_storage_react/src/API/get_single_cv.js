fd324540424be147e64942a3dafa89d11746811a
l1st1k <listik.mp@gmail.com>
2023-04-19 16:00:08 +0300

    done get_single_cv api call

--- cv_storage_react/src/API/get_single_cv.js
+++ cv_storage_react/src/API/get_single_cv.js
@@ -4,7 +4,9 @@ export const get_single_cv = async (cv_id) => {
   try {
     const response = await api.get(`cv/${cv_id}`);
     if (response.status !== 200) throw Error(await response.json());
-    return response.json();
+    return response.json().then((data) => ({
+      data: data,
+    }));
   } catch (err) {
     console.log(err.message);
   }
--- cv_storage_react/src/pages/CVPage.jsx
+++ cv_storage_react/src/pages/CVPage.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import { useParams } from "react-router-dom";
 import { Box, Typography } from "@mui/material";
+
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import { get_single_cv } from "../API";
 
 export const CVPage = () => {
   const { cv_id } = useParams();
+  const [person, setPerson] = useState({});
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await get_single_cv(cv_id);
+      console.log(response);
+      setPerson(response.data);
+      return cv_id;
+    };
+
+    loadData().then((id) => console.log(`Fetched page for ID = ${id}`));
+  }, []);
+
   return (
     <>
       <Box
@@ -26,7 +41,7 @@ export const CVPage = () => {
             textAlign: "center",
           }}
         >
-          CV page(id = {cv_id} ) with detailed information (future)
+          {JSON.stringify(person)}
         </Typography>
         <Footer sx={{ flex: "2" }} />
       </Box>
