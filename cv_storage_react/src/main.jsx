2003c992bfca0f013ba0bd089ce6299fc92c7594
l1st1k <listik.mp@gmail.com>
2023-04-19 15:53:36 +0300

    deleted StrictMode for http_reqs amount reducing

--- cv_storage_react/src/main.jsx
+++ cv_storage_react/src/main.jsx
@@ -5,9 +5,7 @@ import "./assets/styles/index.css";
 import { AppRouter } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>
 );
