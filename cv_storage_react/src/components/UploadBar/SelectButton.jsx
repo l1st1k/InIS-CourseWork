c60b1064930da0fe4d1fc4c39289446b5eb082a9
l1st1k <listik.mp@gmail.com>
2023-04-29 00:29:16 +0300

    replaced alerts with toaster

--- cv_storage_react/src/components/UploadBar/SelectButton.jsx
+++ cv_storage_react/src/components/UploadBar/SelectButton.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button } from "@mui/material";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
+import { toast } from "react-toastify";
 
 export const SelectButton = ({ file, setFile }) => {
+  const toastId = useRef(null);
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
+
     if (selectedFile.type !== "text/csv") {
-      alert("Only CSV files are allowed!");
+      toastId.current = toast.loading("Processing...", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+      toast.update(toastId.current, {
+        autoClose: 3000,
+        render: "Only .csv files are allowed!",
+        type: toast.TYPE.ERROR,
+        isLoading: false,
+      });
+
       return;
     }
     setFile(selectedFile);
--- cv_storage_react/src/components/UploadBar/UploadButton.jsx
+++ cv_storage_react/src/components/UploadBar/UploadButton.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import SendIcon from "@mui/icons-material/Send";
 import { Button } from "@mui/material";
 import { post_cv } from "../../API";
 import { useToaster } from "../../hooks";
+import { toast } from "react-toastify";
 
 export const UploadButton = ({ file }) => {
+  const toastId = useRef(null);
+
   const formData = new FormData();
   formData.append("file", file);
 
@@ -16,7 +19,15 @@ export const UploadButton = ({ file }) => {
   );
   const handleUpload = async () => {
     if (!file) {
-      alert("Please select a file to upload!");
+      toastId.current = toast.loading("Processing...", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+      toast.update(toastId.current, {
+        autoClose: 3000,
+        render: "Please select a file to upload!",
+        type: toast.TYPE.ERROR,
+        isLoading: false,
+      });
       return;
     }
 
