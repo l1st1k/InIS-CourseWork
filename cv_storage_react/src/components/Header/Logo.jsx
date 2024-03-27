e063405b65619b9cebef69debe58ceea16b7d3af
Aliaksandr Listvanovich <aliaksandr.listvanovich@superbo.ai>
2024-03-27 18:34:44 +0200

    site icon

--- cv_storage_react/src/components/Header/Logo.jsx
+++ cv_storage_react/src/components/Header/Logo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as LogoOnWhite } from "../../assets/img/logo_on_white.svg";
+import { ReactComponent as LogoOnWhite } from "../../assets/svg/logo_on_white.svg";
 
 export const Logo = () => {
   return (
