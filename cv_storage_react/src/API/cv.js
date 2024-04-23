a4feaeb80327a190042dbc16abe3b0cea1376247
Aliaksandr Listvanovich <aliaksandr.listvanovich@superbo.ai>
2024-04-23 21:26:19 +0300

    cv API

---
+++ cv_storage_react/src/API/cv.js
@@ -0,0 +1,105 @@
+import {api} from "./api_base.js";
+
+export const get_all_cvs = async () => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/cvs`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cooookie': document.cookie
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch CVs');
+    }
+
+    return response.json();
+  } catch (err) {
+    console.log(err.message);
+    return [];
+  }
+};
+
+export const get_single_cv = async (cv_id) => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/cv/${cv_id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cooookie': document.cookie
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch CV - ${cv_id}`);
+    }
+    return response.json().then((data) => ({
+      data: data,
+    }));
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const get_csv = async (cv_id) => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/cv/${cv_id}/csv`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cooookie': document.cookie
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to download .csv CV - ${cv_id}`);
+    }
+    return response.blob();
+  } catch (err) {
+    console.log(err.message);
+    return "Failed";
+  }
+};
+
+export const delete_cv = async (cv_id) => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/cv/${cv_id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cooookie': document.cookie
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to delete CV - ${cv_id}`);
+    }
+
+    return response.json();
+  } catch (err) {
+    console.log(err.message);
+    return "Failed";
+  }
+};
+
+export const post_cv = async (data) => {
+  try {
+
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/cv`, {
+      method: 'POST',
+      headers: {
+        'Cooookie': document.cookie
+      },
+      body: data,
+
+    });
+    if (response.status !== 201) {
+      throw new Error(`Failed to post CV}`);
+    }
+    return response.status;
+  } catch (err) {
+    console.log(err.message);
+    return "Failed";
+  }
+};
--- cv_storage_react/src/API/index.js
+++ cv_storage_react/src/API/index.js
@@ -1,10 +1,6 @@
 export { api } from "./api_base.js";
-export { get_all_cvs } from "./get_all_cvs.js";
-export { get_single_cv } from "./get_single_cv.js";
-export { get_csv } from "./get_csv.js";
-export { delete_cv } from "./delete_cv.js";
-export { post_cv } from "./post_cv.js";
-export { auth_login, auth_logout, auth_refresh } from "./auth.js";
-export { company_register } from "./company.js";
-
 export { cvs, single_cv_example } from "./temp_data.js";
+
+export { auth_login, auth_logout, auth_refresh } from "./auth.js";
+export { get_all_cvs, get_single_cv, get_csv, delete_cv, post_cv } from "./cv.js";
+export { company_register } from "./company.js";
